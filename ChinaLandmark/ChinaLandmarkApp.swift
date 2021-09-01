8942b05d06c36166a4de08ac3a60f0b9e24ea442
dsz <364615401@qq.com>
2021-09-01 20:38:20 +0800

    Initial Commit

---
+++ ChinaLandmark/ChinaLandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChinaLandmarkApp.swift
+//  ChinaLandmark
+//
+//  Created by dsz on 2021/9/1.
+//
+
+import SwiftUI
+
+@main
+struct ChinaLandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
